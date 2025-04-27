e809aafa9ae8cd138b7078142502c6272197ac59
HienWii <hiennguyen20020002@gmail.com>
2025-04-27 18:06:42 +0700

    27/04

--- unloged/app-js/shop.js
+++ unloged/app-js/shop.js
@@ -91,3 +91,24 @@ import {
   getDocs,
 } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 import { db } from "./config.js";
+const gamesContainer = document.querySelector("#games-list");
+const loadGamesSnapshot = await getDocs(collection(db, "All games"));
+loadGamesSnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+  gamesContainer.innerHTML += `
+  <div class="col-lg-3 col-md-6 align-self-center mb-30 trending-items col-md-6 adv">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/trending-01.jpg" alt=""></a>
+              <span class="price"><em>$36</em>$24</span>
+            </div>
+            <div class="down-content">
+              <span class="category">Action</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html"><i class="fa fa-shopping-bag"></i></a>
+            </div>
+          </div>
+        </div>
+            
+  `;
+});
